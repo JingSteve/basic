5ea3b876d7f3fbe41dc290c02868a291da2e056a
JingSteve <2331801653@qq.com>
2023-08-14 16:56:03 +0800

    2023-08-14-创建一个简单的Maven工程

---
+++ javamavenTest/org/bigdata/helloworld.java
@@ -0,0 +1,13 @@
+package org.bigdata;
+
+/**
+ * @author Administrator * @date 2023/8/14 0014 16:24
+ * @description
+ */
+class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+}
