f272626f07eef459953ed9b2203c669de16c60c8
JingSteve <2331801653@qq.com>
2023-08-14 17:05:11 +0800

    2023-08-14-Hello World修改打印输出

--- javamavenTest/org/bigdata/helloworld.java
+++ javamavenTest/org/bigdata/helloworld.java
@@ -6,7 +6,7 @@ package org.bigdata;
  */
 class helloworld {
     public static void main(String[] args) {
-        System.out.println("1234");
+        System.out.println("12345");
         System.out.println("Hello World!");
     }
 
