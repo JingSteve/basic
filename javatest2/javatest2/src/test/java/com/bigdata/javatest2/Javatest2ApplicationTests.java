2c215b75c4febfa31a2619b703426cc9524d2b9b
JingSteve <2331801653@qq.com>
2023-08-15 10:47:20 +0800

    2023-08-14-增加一个简单的springboot项目

---
+++ javatest2/javatest2/src/test/java/com/bigdata/javatest2/Javatest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdata.javatest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javatest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
